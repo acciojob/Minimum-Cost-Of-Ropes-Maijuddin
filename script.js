365805331e1412b7b2d5e6a8cb209e5d78ecf19c
Maijuddin Mallick <124431155+Maijuddin@users.noreply.github.com>
2023-07-21 19:28:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,24 @@
-function compare(a, b) {
-  if (a < b) {
-      return -1;
-  } else if (a > b) {
-      return 1;
-  } else {
-      return 0;
-  }
-}
- 
- 
- 
 function calculateMinCost() {
   //your code here
-  let str=document.getElementById('rope-lengths').value.split(",");
-  let arr=str.map((str)=>parseInt(str));
-  // console.log(arr);
-  
-let total=0;
-arr=arr.sort(compare);
-// console.log("arr ",arr);
-while(arr.length>=2){
-  let sum=arr[0]+arr[1];
-  // console.log(sum);
-  let rem=[];
-  rem.push(sum);
-  for(let k=2;k<arr.length;k++){
-    rem.push(arr[k]);
-  }
-  rem=rem.sort(compare);
-  // console.log("rem ",rem);
-  arr=[...rem];
-  // console.log("arr ",arr);
-  total=total+sum;
-  // console.log("total "+total+"----------------------");
-}
-console.log(total);
- 
- 
-let result=document.getElementById('result');
-result.innerHTML=total;
+	let form = document.getElementById("form");
+	let ropes = document.getElementById("rope-lengths").value;
+	let result = document.getElementById("result");
+	let ropeLen = ropes.split(',');
+	ropeLen.sort((a,b)=>{return a-b});
+	// console.log(ropes, ropeLen);
+	let all = [];
+	ropeLen.forEach((e)=>{
+		let i = parseInt(e);
+		all.push(i);
+	});
+	let res = 0;
+	while(all.length>1){
+		let a = all.shift();
+		let b = all.shift();
+		res += a+b;
+		all.push(a+b);
+		all.sort((a,b)=>{return a-b});
+	}
+	// console.log(res);
+	result.innerText = res;	
 }  
